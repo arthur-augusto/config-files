4bdb5f61bd9d1d8522012c0370b9452053fc12e0
arthur-augusto <arthur.augusto.rp@gmail.com>
2023-10-30 07:22:39 -0300

    Adicionando configuração notebook

---
+++ laptop/copia-arquivos.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+cp ./i3/config ~/.config/i3/
+cp ./i3status/i3status.conf ~/.config/i3status
+cp ./nvim/init.vim ~/.config/nvim
+cp ./xorg/.xinitrc ~/
+cp ./xorg/00-keyboard.conf /etc/X11/xorg.conf.d/
+cp ./xorg/10-monitor.conf /etc/X11/xorg.conf.d/
