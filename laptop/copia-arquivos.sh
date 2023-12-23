1524e008aab9f4914a7554cbb24d0a8fd65447d6
arthur-augusto <arthur.augusto.rp@gmail.com>
2023-12-23 20:37:20 -0300

    Adicionando configuracao do touchpad em diretorios e script

--- laptop/copia-arquivos.sh
+++ laptop/copia-arquivos.sh
@@ -6,3 +6,4 @@ cp ./nvim/init.vim ~/.config/nvim
 cp ./xorg/.xinitrc ~/
 cp ./xorg/00-keyboard.conf /etc/X11/xorg.conf.d/
 cp ./xorg/10-monitor.conf /etc/X11/xorg.conf.d/
+cp ./xorg/30-touchpad.conf /etc/X11/xorg.conf.d/
